e2fb1ac242f7f2193b1f455ad4f6108fde80fa71
cxnvti <jenishbekov24bj@gmail.com>
2024-01-17 00:02:59 +0600

    1hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const user = {
+  name: "Shelly",
+  imageUrl: "https://preview.redd.it/is-star-shelly-objectively-the-rarest-skin-in-the-game-im-v0-hrjcyd8ybaxa1.jpg?auto=webp&s=507010d52c39a35473e93187b7cbe72fe2a9387e",
+  imageSize: 90,
+};
+
+export default function Profile() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of" + user.name}
+        style={{ width: user.imageSize, height: user.imageSize }}
+      />
+    </>
   );
 }
-
-export default App;
